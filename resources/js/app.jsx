29aa40e1df4771ae72075575a0818d2313b4b714
Sharma <67532536+sharma2206@users.noreply.github.com>
2026-08-13 12:10:14 +0530

    Bug fixes

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import UploadPage from './pages/UploadPage';
-import DashboardPage from './pages/DashboardPage';
+import "./bootstrap";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UploadPage from "./pages/UploadPage";
+import DashboardPage from "./pages/DashboardPage";
 
-export default function App() {
+function App() {
     return (
         <BrowserRouter>
             <Routes>
@@ -13,3 +15,5 @@ export default function App() {
         </BrowserRouter>
     );
 }
+
+createRoot(document.getElementById("root")).render(<App />);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
 
